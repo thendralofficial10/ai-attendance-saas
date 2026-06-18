ad0a6164e3eb74da7e11d776a905028f6befbcbb
Thendral10 <ilanthendralraju@gmail.com>
2026-06-18 23:32:03 +0530

    chore: initialize frontend architecture

---
+++ frontend/app/page.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold">
+          AI Attendance SaaS
+        </h1>
+
+        <p className="mt-4">
+          Workforce Management Platform
+        </p>
+      </div>
+    </main>
+  );
+}
